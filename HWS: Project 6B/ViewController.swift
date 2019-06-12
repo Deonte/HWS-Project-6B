f98fec36e7c25c8717e1bbf8bc8fd91b791f2c66
Deonte <dkill51@gmail.com>
2019-06-12 17:06:44 -0400

    Initial Commit

---
+++ HWS: Project 6B/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HWS: Project 6B
+//
+//  Created by Deonte on 6/12/19.
+//  Copyright © 2019 Deonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
